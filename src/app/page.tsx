c9fce3b60807744783678cd45af04cc7fe9f0336
Mike Dyer <mike@chindogulabs.com>
2025-10-19 21:03:42 -0400

    add nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Signup</h1>
+      <Image src="/logo.png" alt="Logo" width={200} height={200} />
+    </main>
+  );
+}
